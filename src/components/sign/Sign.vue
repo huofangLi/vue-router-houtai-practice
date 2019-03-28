d86c52a95402705751879a12bc96dc31d67c6e1f
wufei0429 <2453804076@qq.com>
2019-03-28 14:09:27 +0800

    sign

--- src/components/sign/Sign.vue
+++ src/components/sign/Sign.vue
@@ -1,86 +1,58 @@
 <template>
-	<div class="container">
+	<div>
 		<div class="item-logo">
-			<router-link to="/index"><img src="../../assets/sign-bg.svg" class="logo" /></router-link>
+			<router-link to="/index">
+				<img src="../../assets/sign-bg.svg" class="logo" />
+			</router-link>
 		</div>
-		
-		<div class="item">
-			<div class="item-sign">
-				<div class="signin">
-					<router-link to="/sign_in" class="signin2">登录</router-link>
-				</div>
-				<div class="signup">
-					<router-link to="/sign_up" class="signup2">注册</router-link>
-				</div>
-			</div>
-			<div class="item-content">
-				<router-view/>
-			</div>
+
+		<div class="container"></div>
+		<div class="sign-box">
+			<router-link to="/sign_up" text-align:center>注册</router-link>
+			<router-link to="/sign_in">登录</router-link>
+			<router-view />
 		</div>
-		
 	</div>
 </template>
 
 <script>
-	export default {
-		name: 'Sign',
-		data() {
-			return {};
-		}
-	};
+export default {
+	name: 'Sign',
+	data() {
+		return {};
+	}
+};
 </script>
 
 <style>
-	.container{
-		background-color: #FFFFFF;
-		margin: 0 auto;
-	}
-	
-	.item-logo{
-		margin-top: 50px;
-	}
-	
-	.item{
-		width: 400px;
-		height: 400px;
-		margin-top: 200px;
-		margin-left: 1300px;
-		background-color: aqua; 
-	}
-	
-	.item-sign{
-		display: flex;
-		height: 10%;
-		width: 100%;
-		background-color: #42B983; 
-		
-		align-items: center;
-		/* 前提是flex布局 垂直方向居中 */
-		justify-content: center;
-		/* 水平方向居	中 */
-	}
-	
-	.item-content{
-		width: 100%;
-		height: 90%;
-		margin: 0 auto;
-	}
-	
-	.signin{
-		width: 50%;
-	}
-	
-	.signup{
-		width: 50%;
-	}
-	
-	.signin2{
-		/* color: #FFFFFF; */
-		text-decoration: none;
-	}
-	
-	.signup2{
-		/* color: #FFFFFF; */
-		text-decoration: none;
-	}
+.item-logo {
+	margin-top: 50px;
+	width: 100%;
+	height: 80px;
+	background-color: #fff;
+}
+.logo {
+	width: 200px;
+	height: 50px;
+	margin-left: 100px;
+}
+.container {
+	width: 100%;
+	height: 800px;
+	background-image: url(../../assets/login-bg.jpg);
+	background-size: 100%;
+}
+.sign-box {
+	position: absolute;
+	bottom: 120px;
+	right: 200px;
+	width: 500px;
+	height: 400px;
+	background-color: #fff;
+}
+a {
+	margin-right: 200px;
+	font-size: 16pt;
+	color: black;
+}
 </style>
--- src/components/sign/SignIn.vue
+++ src/components/sign/SignIn.vue
@@ -1,17 +1,81 @@
 <template>
-	<div>
-		<h2>登录</h2>
+	<div class="content">
+		<div class="si">
+			<p class="p">
+				<i class="icon-asterisk"></i>
+				用户名:
+			</p>
+			<input v-model="name" placeholder="	用户名" class="a" />
+		</div>
+		<div class="si">
+			<p class="p">
+				<i class="icon-asterisk"></i>
+				密码:
+			</p>
+			<input v-model="password1" placeholder="	密码" class="a" />
+		</div>
+		<div class="xia">
+			<button type="button" @click="commit" class="g"><h3>登录</h3></button>
+		</div>
 	</div>
 </template>
 
 <script>
-	export default {
-		name: 'SignIn',
-		data() {
-			return {};
+export default {
+	name: 'SignIn',
+	data() {
+		return { name: '', password1: '' };
+	},
+	methods: {
+		commit: function() {
+			alert();
 		}
-	};
+	}
+};
 </script>
 
 <style>
+.content {
+	width: 90%;
+	height: 90%;
+	margin: 0 auto;
+	margin-top: 40px;
+	align-items: center;
+}
+.p {
+	flex: 1 1 30%;
+}
+.si {
+	display: flex;
+	width: 90%;
+	height: 60px;
+	border-radius: 10px;
+	outline: none;
+}
+.a {
+	flex: 1 1 70%;
+	width: 200px;
+	height: 40px;
+	border-radius: 10px;
+
+	border: 1px solid #dddddd;
+	outline: none;
+}
+.xia {
+	padding-top: 40px;
+	width: 200px;
+	height: 50px;
+	align-items: center;
+	padding-left: 25%;
+}
+
+.g {
+	width: 200px;
+	height: 50px;
+	border-radius: 10px;
+	border: 1px solid #dddddd;
+	outline: none;
+	background-color: rgb(169, 89, , 228);
+	color: #eeeeee;
+}
 </style>
